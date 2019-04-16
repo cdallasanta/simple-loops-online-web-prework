7669fece90f5cc7c74129a8c46a19c923aa75b6e
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-16 18:26:51 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,6 +11,12 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   loop do
+    puts phrase
+    counter += 1
+    if counter = number_of_times
+      break
+    end
+  end
 
 end
 
